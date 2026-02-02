f1a3e0e68a0f9d55b194185331262b2269c796b5
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2026-02-02 13:24:48 -0600

    Geeks for Geeks Tutorial
    https://www.geeksforgeeks.org/flutter/multi-page-applications-in-flutter/

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,18 +2,95 @@ import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const HomeRoute(),
+      '/second': (context) => const SecondRoute(),
+      '/third': (context) => const ThirdRoute(),
+    },
+    debugShowCheckedModeBanner: false,
+  )); //MaterialApp
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HomeRoute extends StatelessWidget {
+  const HomeRoute({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Geeks for Geeks'),
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+      ), // AppBar
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(Colors.green),
+                  foregroundColor: WidgetStateProperty.all(Colors.white)),
+              child: const Text('Click Me!'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/second');
+              },
+            ), // ElevatedButton
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(Colors.green),
+                  foregroundColor: WidgetStateProperty.all(Colors.white)),
+              child: const Text('Tap Me!'),
+              onPressed: () {
+                Navigator.pushNamed(context, '/third');
+              },
+            ), // ElevatedButton
+          ], // <Widget>[]
+        ), // Column
+      ), // Center
+    ); // Scaffold
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Click Me Page"),
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+      ), // AppBar
+      body: Center(
+        child: ElevatedButton(
+          style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all(Colors.green),
+              foregroundColor: WidgetStateProperty.all(Colors.white)),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Back!'),
+        ), // ElevatedButton
+      ), // Center
+    ); // Scaffold
+  }
+}
+
+class ThirdRoute extends StatelessWidget {
+  const ThirdRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Tap Me Page"),
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+      ), // AppBar
+    ); // Scaffold
   }
 }
 
